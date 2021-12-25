e926584eccf7e55ec87a5cc7bcd8452de7007f4f
Fredric Agerling <f.agerling@hotmail.com>
2021-12-25 15:12:19 +0100

    Add GenericRepository and UnitOfWork

---
+++ HotelListing/Repository/IGenericRepository.cs
@@ -0,0 +1,20 @@
+﻿using System.Linq.Expressions;
+
+namespace HotelListing.Repository
+{
+    public interface IGenericRepository<T> where T : class
+    {
+        Task<List<T>> GetAllAsync(Expression<Func<T, bool>> expression = null,
+            Func<IQueryable<T>, IOrderedQueryable<T>> orderBy = null,
+            List<string> includes = null
+        );
+
+        Task<T> GetAsync(Expression<Func<T, bool>> expression, List<string> includes = null);
+
+        Task AddAsync(T entity);
+        Task AddRangeAsync(IEnumerable<T> entities);
+        Task DeleteAsync(int id);
+        void DeleteRangeAsync(IEnumerable<T> entities);
+        void Update(T entity);
+    }
+}
---
+++ HotelListing/Repository/IUnitOfWork.cs
@@ -0,0 +1,11 @@
+﻿using HotelListing.Data;
+
+namespace HotelListing.Repository
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        IGenericRepository<Country> Countries { get; }
+        IGenericRepository<Hotel> Hotels { get; }
+        Task Save();
+    }
+}
